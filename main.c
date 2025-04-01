f3b35e1b10ee8f2e9f1096b61699599bb59ce086
hailey! <hailey@---.com>
2025-04-01 06:59:51 -0500

    append line

--- main.c
+++ main.c
@@ -5,101 +5,92 @@
 #include <stdlib.h>
 #include <string.h>
 
-void FreeAllLines(char **lines, int count)
-{
-    for (int i = 0; i < count; i++)
-    {
-        free(lines[i]);
-    }
+void FreeAllLines(char **lines, int count) {
+  for (int i = 0; i < count; i++) {
+    free(lines[i]);
+  }
 
-    free(lines);
+  free(lines);
 }
-char **ReadAllLines(const char *fname, int *out_linecount)
-{
-    FILE *f = fopen(fname, "r");
 
-    if (f == NULL)
-        return NULL;
+char **ReadAllLines(const char *fname, int *out_linecount) {
+  FILE *f = fopen(fname, "r");
 
-    int arraylen = 1024;
-    int strlen = 1024;
+  if (f == NULL)
+    return NULL;
 
-    int lines_used = 0;
-    int chars_read = 0;
+  int arraylen = 1024;
+  int strlen = 1024;
 
-    char **ret = malloc(sizeof(char *) * arraylen);
-    ret[lines_used] = malloc(strlen);
+  int lines_used = 0;
+  int chars_read = 0;
 
-    while (1)
-    {
+  char **ret = malloc(sizeof(char *) * arraylen);
+  ret[lines_used] = malloc(strlen);
 
-        if (feof(f))
-        {
-            // we are all done reading, finlise the last line
-            ret[lines_used] = realloc(ret[lines_used], chars_read); // shrink string to be size read
-            ret[lines_used][chars_read - 1] = 0;                    // terminate the string
-            lines_used++;
-            break;
-        }
-
-        char c = fgetc(f);
-
-        if (ferror(f))
-        {
-            perror("fgetc");
-            return NULL;
-        }
-
-        if (chars_read > strlen)
-        {
-            strlen *= 2;
-            ret[lines_used] = realloc(ret[lines_used], strlen);
-        }
-
-        if (c == '\n')
-        {
-            // store this line
-
-            ret[lines_used] = realloc(ret[lines_used], chars_read + 1); // shrink string to be size read
-            ret[lines_used][chars_read] = 0;                            // terminate the string
-
-            strlen = 1024; // reset var
-            chars_read = 0;
-
-            lines_used += 1;
-
-            if (lines_used >= arraylen) // grow the continaing array
-            {
-                arraylen *= 2;
-                ret = realloc(ret, arraylen * sizeof(char *));
-            }
-            ret[lines_used] = malloc(strlen); // alloc the next string
-            continue;
-        }
-        else if (c == '\r' || c == '\n' && 0 >= chars_read)
-        {
-            // we just got a stray \r or \n
-            continue;
-        }
-
-        ret[lines_used][chars_read] = c;
-        chars_read += 1;
+  while (1) {
+    if (feof(f)) {
+      // we are all done reading, finlise the last line
+      ret[lines_used] =
+          realloc(ret[lines_used], chars_read); // shrink string to be size read
+      ret[lines_used][chars_read - 1] = 0;      // terminate the string
+      lines_used++;
+      break;
     }
 
-    // resize the return array to fit used
-    ret = realloc(ret, lines_used * sizeof(char *));
+    char c = fgetc(f);
 
-    *out_linecount = lines_used;
-    return ret;
+    if (ferror(f)) {
+      perror("fgetc");
+      return NULL;
+    }
+
+    if (chars_read > strlen) {
+      strlen *= 2;
+      ret[lines_used] = realloc(ret[lines_used], strlen);
+    }
+
+    if (c == '\n') {
+      // store this line
+
+      ret[lines_used] = realloc(
+          ret[lines_used], chars_read + 1); // shrink string to be size read
+      ret[lines_used][chars_read] = 0;      // terminate the string
+
+      strlen = 1024; // reset var
+      chars_read = 0;
+
+      lines_used += 1;
+
+      if (lines_used >= arraylen) // grow the continaing array
+      {
+        arraylen *= 2;
+        ret = realloc(ret, arraylen * sizeof(char *));
+      }
+      ret[lines_used] = malloc(strlen); // alloc the next string
+      continue;
+    } else if (c == '\r' || c == '\n' && 0 >= chars_read) {
+      // we just got a stray \r or \n
+      continue;
+    }
+
+    ret[lines_used][chars_read] = c;
+    chars_read += 1;
+  }
+
+  // resize the return array to fit used
+  ret = realloc(ret, lines_used * sizeof(char *));
+
+  *out_linecount = lines_used;
+  return ret;
 }
 
 Font g_font;
 float g_font_size;
 
-struct Line
-{
-    char *base;
-    int len, cap;
+struct Line {
+  char *base;
+  int len, cap;
 } *g_lines;
 int g_lines_count;
 
@@ -110,258 +101,240 @@ int g_cursor_line, g_cursor_col;
 
 #define LINENUMBERSUPPORT 3
 
-void GrowString(struct Line *l)
-{
-    int new_cap = (l->cap + 1) * 2;
-    char *new = realloc(l->base, new_cap);
+void GrowString(struct Line *l) {
+  int new_cap = (l->cap + 1) * 2;
+  char *new = realloc(l->base, new_cap);
 
-    l->base = new;
-    l->cap = new_cap;
+  l->base = new;
+  l->cap = new_cap;
 }
 
-void AppendLine()
-{
-    // TODO: make g_lines a dynamic array (have a len and cap)
-    printf("Not implimented: AppendLine()\n");
-    exit(1);
+void AppendLine() {
+  g_lines = realloc(g_lines, sizeof(struct Line) * (g_lines_count + 1));
+  printf("%d\n", g_lines_count);
+  g_lines[g_lines_count].base = 0;
+  g_lines[g_lines_count].cap = 0;
+  g_lines[g_lines_count].len = 0;
+  g_lines_count += 1;
+}
+void Backspace() {
+  struct Line *l = &g_lines[g_cursor_line];
+  
+  if (0 >= l->len)
+    return; // todo: Delete The line we are on
+
+  l->len -= 1;
 }
 
-void InsertChar(char c)
-{
-    if (g_cursor_line >= g_lines_count)
-    {
-        AppendLine();
-    }
+void InsertChar(char c) {
+  if (g_cursor_line >= g_lines_count) {
+    AppendLine();
+  }
 
-    struct Line *l = &g_lines[g_cursor_line];
+  struct Line *l = &g_lines[g_cursor_line];
 
-    if (l->len + 1 > l->cap)
-    {
-        printf("Growing\n");
-        GrowString(l);
-    }
+  if (l->len + 1 > l->cap) {
+    printf("Growing\n");
+    GrowString(l);
+  }
 
-    printf("%p len:%d  cap:%d\n", l->base, l->len, l->cap);
+  printf("%p len:%d  cap:%d\n", l->base, l->len, l->cap);
 
-    l->base[l->len] = c;
-    l->len += 1;
+  l->base[l->len] = c;
+  l->len += 1;
 }
 
-void FDrawText(const char *text, int x, int y, Color color) { DrawTextEx(g_font, text, (Vector2){x, y}, g_font_size, 1, color); }
+void FDrawText(const char *text, int x, int y, Color color) {
+  DrawTextEx(g_font, text, (Vector2){x, y}, g_font_size, 1, color);
+}
 
-void LoadTextFile(const char *filepath)
-{
-    g_cursor_line = 0;
-    g_cursor_col = 0;
+void LoadTextFile(const char *filepath) {
+  g_cursor_line = 0;
+  g_cursor_col = 0;
 
-    int linecount = 0;
-    char **lines = ReadAllLines(filepath, &linecount);
+  int linecount = 0;
+  char **lines = ReadAllLines(filepath, &linecount);
 
-    if (!linecount || !lines)
-        return;
+  if (!linecount || !lines)
+    return;
 
-    g_lines = malloc(sizeof(struct Line) * linecount);
-    if (!g_lines)
-        return;
+  g_lines = malloc(sizeof(struct Line) * linecount);
+  if (!g_lines)
+    return;
 
-    g_lines_count = linecount;
+  g_lines_count = linecount;
 
-    for (size_t i = 0; i < linecount; i++)
-    {
-        char *line = lines[i];
-        g_lines[i].len = g_lines[i].cap = strlen(line);
-        g_lines[i].base = line;
-    }
+  for (size_t i = 0; i < linecount; i++) {
+    char *line = lines[i];
+    g_lines[i].len = g_lines[i].cap = strlen(line);
+    g_lines[i].base = line;
+  }
 
-    SetWindowTitle(TextFormat("%s - FCON", GetFileName(filepath)));
+  SetWindowTitle(TextFormat("%s - FCON", GetFileName(filepath)));
 }
 
 void OnResize() { g_screen_line_count = GetScreenHeight() / g_font_size; }
 
-void OnCurrsorLineChanged()
-{
-    if (g_topline + 5 > g_cursor_line)
-    {
-        g_topline = g_cursor_line - 5;
-    }
+void OnCurrsorLineChanged() {
+  if (g_topline + 5 > g_cursor_line) {
+    g_topline = g_cursor_line - 5;
+  }
 
-    if ((g_topline + g_screen_line_count) - 5 < g_cursor_line)
-    {
-        g_topline = (g_cursor_line - g_screen_line_count) + 5;
-    }
+  if ((g_topline + g_screen_line_count) - 5 < g_cursor_line) {
+    g_topline = (g_cursor_line - g_screen_line_count) + 5;
+  }
 }
 
-int main()
-{
-    InitWindow(800, 600, "FCON");
-    SetTargetFPS(60);
-    EnableEventWaiting();
+int main() {
+  InitWindow(800, 600, "FCON");
+  SetTargetFPS(60);
+  EnableEventWaiting();
 
-    g_font = LoadFont("romulus.png");
-    if (!IsFontValid(g_font))
-    {
-        TraceLog(LOG_WARNING, "Could not load font, using default.");
-        g_font = GetFontDefault();
+  g_font = LoadFont("romulus.png");
+  if (!IsFontValid(g_font)) {
+    TraceLog(LOG_WARNING, "Could not load font, using default.");
+    g_font = GetFontDefault();
+  }
+
+  g_font_size = 12;
+  g_topline = 0;
+  g_lines_count = 0;
+  g_lines = NULL;
+  g_screen_line_count = 0;
+  g_cursor_line = 0;
+  g_cursor_col = 0;
+
+  // LoadTextFile("test_small.fc");
+  LoadTextFile("test.fc");
+
+  OnResize();
+
+  unsigned redraw_count = 0;
+
+  while (!WindowShouldClose()) {
+    redraw_count++;
+
+    if (IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL)) {
+      if (IsKeyPressed(KEY_EQUAL)) {
+        g_font_size += 1.0f;
+        OnResize();
+      }
+
+      if (IsKeyPressed(KEY_MINUS)) {
+        g_font_size -= 1.0f;
+        OnResize();
+      }
+
+      if (IsKeyPressed(KEY_ZERO)) {
+        g_font_size = 12;
+        OnResize();
+      }
+
+      if (IsKeyPressed(KEY_UP) || IsKeyPressedRepeat(KEY_UP)) {
+        g_topline -= 1;
+      }
+
+      if (IsKeyPressed(KEY_DOWN) || IsKeyPressedRepeat(KEY_DOWN)) {
+        g_topline += 1;
+      }
+    } else {
+
+      if (IsKeyPressed(KEY_PAGE_UP) || IsKeyPressedRepeat(KEY_PAGE_UP)) {
+        g_topline -= g_screen_line_count - 5;
+        g_cursor_line -= g_screen_line_count - 5;
+        OnCurrsorLineChanged();
+      }
+
+      if (IsKeyPressed(KEY_PAGE_DOWN) || IsKeyPressedRepeat(KEY_PAGE_DOWN)) {
+        g_topline += g_screen_line_count - 5;
+        g_cursor_line += g_screen_line_count - 5;
+        OnCurrsorLineChanged();
+      }
+
+      if (IsKeyPressed(KEY_UP) || IsKeyPressedRepeat(KEY_UP)) {
+        g_cursor_line -= 1;
+        OnCurrsorLineChanged();
+      }
+
+      if (IsKeyPressed(KEY_DOWN) || IsKeyPressedRepeat(KEY_DOWN)) {
+        g_cursor_line += 1;
+        OnCurrsorLineChanged();
+      }
+
+      if (IsKeyPressed(KEY_BACKSPACE) || IsKeyPressedRepeat(KEY_BACKSPACE)) {
+        Backspace();
+      }
+
+      int c;
+      do {
+        c = GetCharPressed();
+        if (!c)
+          break;
+
+        InsertChar(c);
+      } while (c);
     }
 
-    g_font_size = 12;
-    g_topline = 0;
-    g_lines_count = 0;
-    g_lines = NULL;
-    g_screen_line_count = 0;
-    g_cursor_line = 0;
-    g_cursor_col = 0;
-
-    // LoadTextFile("test_small.fc");
-    LoadTextFile("test.fc");
-
-    OnResize();
-
-    unsigned redraw_count = 0;
-
-    while (!WindowShouldClose())
-    {
-        redraw_count++;
-
-        if (IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL))
-        {
-            if (IsKeyPressed(KEY_EQUAL))
-            {
-                g_font_size += 1.0f;
-                OnResize();
-            }
-
-            if (IsKeyPressed(KEY_MINUS))
-            {
-                g_font_size -= 1.0f;
-                OnResize();
-            }
-
-            if (IsKeyPressed(KEY_ZERO))
-            {
-                g_font_size = 12;
-                OnResize();
-            }
-
-            if (IsKeyPressed(KEY_UP) || IsKeyPressedRepeat(KEY_UP))
-            {
-                g_topline -= 1;
-            }
-
-            if (IsKeyPressed(KEY_DOWN) || IsKeyPressedRepeat(KEY_DOWN))
-            {
-                g_topline += 1;
-            }
-        }
-        else
-        {
-
-            if (IsKeyPressed(KEY_PAGE_UP) || IsKeyPressedRepeat(KEY_PAGE_UP))
-            {
-                g_topline -= g_screen_line_count - 5;
-                g_cursor_line -= g_screen_line_count - 5;
-                OnCurrsorLineChanged();
-            }
-
-            if (IsKeyPressed(KEY_PAGE_DOWN) || IsKeyPressedRepeat(KEY_PAGE_DOWN))
-            {
-                g_topline += g_screen_line_count - 5;
-                g_cursor_line += g_screen_line_count - 5;
-                OnCurrsorLineChanged();
-            }
-
-            if (IsKeyPressed(KEY_UP) || IsKeyPressedRepeat(KEY_UP))
-            {
-                g_cursor_line -= 1;
-                OnCurrsorLineChanged();
-            }
-
-            if (IsKeyPressed(KEY_DOWN) || IsKeyPressedRepeat(KEY_DOWN))
-            {
-                g_cursor_line += 1;
-                OnCurrsorLineChanged();
-            }
-
-            int c;
-            do
-            {
-                c = GetCharPressed();
-                if (!c)
-                    break;
-
-                InsertChar(c);
-            } while (c);
-        }
-
-        if (IsWindowResized())
-        {
-            OnResize();
-        }
-
-        Vector2 scroll = GetMouseWheelMoveV();
-        g_topline -= scroll.y * 2;
-
-        if (g_topline + g_screen_line_count > g_lines_count)
-        {
-            g_topline = g_lines_count - g_screen_line_count;
-        }
-
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-        {
-            Vector2 pt = GetMousePosition();
-
-            for (size_t i = g_topline; i < (g_screen_line_count + g_topline); i++)
-            {
-                Rectangle r = {
-                    g_font_size * LINENUMBERSUPPORT - 1,
-                    (i - g_topline) * g_font_size,
-                    GetScreenWidth(),
-                    g_font_size};
-
-                if (CheckCollisionPointRec(pt, r))
-                {
-                    g_cursor_line = i;
-                    break;
-                }
-            }
-        }
-
-        if (0 > g_topline)
-            g_topline = 0;
-
-        if (0 > g_cursor_line)
-            g_cursor_line = 0;
-
-        if (g_cursor_line >= g_lines_count)
-            g_cursor_line = g_lines_count;
-
-        BeginDrawing();
-        ClearBackground((Color){0xfd, 0xf6, 0xe3, 0xFF});
-
-        for (size_t i = g_topline; i < (g_screen_line_count + g_topline); i++)
-        {
-            FDrawText(TextFormat("%*d", LINENUMBERSUPPORT, i + 1), 3, (i - g_topline) * g_font_size, BLACK);
-
-            if (i == g_cursor_line)
-            {
-                DrawRectangle(g_font_size * LINENUMBERSUPPORT - 1,
-                              (i - g_topline) * g_font_size,
-                              GetScreenWidth(),
-                              g_font_size,
-                              WHITE);
-            }
-
-            if (i >= g_lines_count)
-                break;
-            FDrawText(TextFormat("%.*s", g_lines[i].len, g_lines[i].base),
-                      g_font_size * LINENUMBERSUPPORT - 1, (i - g_topline) * g_font_size, BLACK);
-        }
-
-        DrawText(TextFormat("TOP IDX: %d\n%u", g_topline, redraw_count),
-                 40, GetScreenHeight() - g_font_size * 2, g_font_size, PURPLE);
-
-        EndDrawing();
+    if (IsWindowResized()) {
+      OnResize();
     }
 
-    return 0;
+    Vector2 scroll = GetMouseWheelMoveV();
+    g_topline -= scroll.y * 2;
+
+    if (g_topline + g_screen_line_count > g_lines_count) {
+      g_topline = g_lines_count - g_screen_line_count;
+    }
+
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+      Vector2 pt = GetMousePosition();
+
+      for (size_t i = g_topline; i < (g_screen_line_count + g_topline); i++) {
+        Rectangle r = {g_font_size * LINENUMBERSUPPORT - 1,
+                       (i - g_topline) * g_font_size, GetScreenWidth(),
+                       g_font_size};
+
+        if (CheckCollisionPointRec(pt, r)) {
+          g_cursor_line = i;
+          break;
+        }
+      }
+    }
+
+    if (0 > g_topline)
+      g_topline = 0;
+
+    if (0 > g_cursor_line)
+      g_cursor_line = 0;
+
+    if (g_cursor_line >= g_lines_count)
+      g_cursor_line = g_lines_count;
+
+    BeginDrawing();
+    ClearBackground((Color){0xfd, 0xf6, 0xe3, 0xFF});
+
+    for (size_t i = g_topline; i < (g_screen_line_count + g_topline); i++) {
+      FDrawText(TextFormat("%*d", LINENUMBERSUPPORT, i + 1), 3,
+                (i - g_topline) * g_font_size, BLACK);
+
+      if (i == g_cursor_line) {
+        DrawRectangle(g_font_size * LINENUMBERSUPPORT - 1,
+                      (i - g_topline) * g_font_size, GetScreenWidth(),
+                      g_font_size, WHITE);
+      }
+
+      if (i >= g_lines_count)
+        break;
+      FDrawText(TextFormat("%.*s", g_lines[i].len, g_lines[i].base),
+                g_font_size * LINENUMBERSUPPORT - 1,
+                (i - g_topline) * g_font_size, BLACK);
+    }
+
+    DrawText(TextFormat("TOP IDX: %d\n%u", g_topline, redraw_count), 40,
+             GetScreenHeight() - g_font_size * 2, g_font_size, PURPLE);
+
+    EndDrawing();
+  }
+
+  return 0;
 }
